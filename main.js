1bc008cc922011c548cc99abaf5091bc25ef3a1e
rachele <rachele.terzoli@gmail.com>
2020-04-13 17:39:39 +0200

    funzionante

--- main.js
+++ main.js
@@ -1,34 +1,49 @@
 function azzera(){
-document.getElementById("mex").value = "";
+  document.getElementById("mex").value = "";
 }
 function cancella(){
-document.getElementById("lente").value = "";
-$(".icone" ).show();
+  document.getElementById("lente").value = "";
+  $(".icone" ).show();
 }
 $(document).ready(function(){
 
 
-  var chat = $('.messaggini');
   var inputMsg = $('#mex');
   var buttonSend = $('.invia');
 
   //gestisco evento su bottone di invio
   buttonSend.click(
     function () {
+      var chat = $('.messaggini.active');
+      var onclick_verde = 'erase(this , "verde" )';
+      var onclick_bianco = 'erase(this , "bianco" )';
+      var dropdown_menu = "<div class='tendina'><i class='fas fa-chevron-down'></i><div class='menu'><div onclick ='"+onclick_verde+"'>Cancella il messaggio </div></div></div>";
       var msg = inputMsg.val();
-      chat.append('<div class="verde">' + msg + '</div>');
+      chat.append('<div class="verde">' + msg + dropdown_menu +'  </div>');
+
+      $(this).append(dropdown_menu);
       inputMsg.val("");
 
       //dopo un secondo
       // deve apparire un nuovo msg con un testo sempre uguale (statico)
       setTimeout(function(){
-        chat.append('<div class="bianco">' + "ok" + '</div>');
+        var dropdown_menu = "<div class='tendina'><i class='fas fa-chevron-down'></i><div class='menu'><div onclick ='"+onclick_bianco+"'>Cancella il messaggio </div></div></div>";
+        chat.append('<div class="bianco">' + "ok" + dropdown_menu + '</i></div>');
       }, 1000);
 
 
     });
 
+    $(".identity").click(function(){
+      $(".messaggini").removeClass("active");
+      //far sparire tutto
+      $(".messaggini").hide();
+      //far vedre solo id giusto
+      var amico = $(this).data("nome");
+      $("#"+ amico).show();
+      $("#" + amico).addClass("active");
 
+    });
 
     //funzione Cerca
     //ciclo .each() .identity >.nero
@@ -43,16 +58,21 @@ $(document).ready(function(){
         var nome = $(this).find(".identity >.nero").text();
 
         if(nome.includes(inputSearch) === false){
-            console.log(nome+" false");
-           $(this).hide();
+          console.log(nome+" false");
+          $(this).hide();
         }else{
-            console.log(nome);
-            $(this).show();
+          console.log(nome);
+          $(this).show();
         }
-
-
-
       });
+
+    });
+
+
+
   });
 
-});
+  function erase(ele, classe){
+    console.log(ele);
+    ele.closest( "."+classe ).remove();
+  }
